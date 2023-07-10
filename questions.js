107f81826331ac1a4313913882550039e23a5c15
Swagata Nandi <98303519+Swagata0410@users.noreply.github.com>
2023-07-10 20:55:17 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who is making the web standards?",
+    answer: "World Wide Web",
+    options: [
+      "Google",
+      "Mozilla",
+      "World Wide Web",
+      "Microsoft"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which HTML attribute is used to define inline styles?",
+    answer: "style",
+    options: [
+      "class",
+      "style",
+      "styles",
+      "font"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do insert a comment in CSS file?",
+    answer: "/* this is a comment",
+    options: [
+      "// this a comment",
+      "^ this is a comment",
+      "/* this is a comment",
+      "// this is comment// "
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which character is used to indicate an end tag?",
+    answer: "/",
+    options: [
+      "*",
+      "/",
+      ">",
+      "^"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which property is used to change the background color? ",
+    answer: "background-color",
+    options: [
+      "background-color",
+      "bgcolor",
+      "color",
+      "background"
+    ]
+  },
+  {
+    numb: 8 ,
+    question: "How do you select all the columns from a table name (students)?",
+    answer: "SELECT * FROM students",
+    options: [
+      "SELECT [all] FROM students",
+      "SELECT * FROM students",
+      "SELECT students",
+      "SELECT * students"
+    ]
+  },
+  {
+    numb: 9 ,
+    question: "Which SQL keyword is used to sort the result-set?",
+    answer: "None of the above",
+    options: [
+      "None of the above",
+      "SORT",
+      "ORDER ",
+      "SORT BY"
+    ]
+  },
+  {
+    numb: 10 ,
+    question: "What is the most common type of join used?",
+    answer: "INNER JOIN",
+    options: [
+      "JOINED TABLE",
+      "INNER JOIN",
+      "INSIDE JOIN",
+      "JOIN"
+    ]
+  },
+ 
+
+];
